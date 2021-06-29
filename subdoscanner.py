48a0b12fe0db56f9af4f7482ab3cc6a9d25cce7e
dzakialthalsyah <dzakialthalsy@gmail.com>
2021-06-29 22:53:50 +0700

    first commit

---
+++ subdoscanner.py
@@ -0,0 +1,52 @@
+from termcolor import colored
+from pyfiglet import figlet_format
+
+print((colored(figlet_format("SUBDOMAIN SCANNER", font="future_1"), color="red")))
+print((colored("-------------- Coded by Dzaki Althalsyah -------------- \n\n", color="blue")))
+
+# import module requests
+import requests
+
+# function untuk scanning subdomain
+def subscanner(scan_nama_domain, scan_nama_subdomain):
+
+    print("---------------- Hasil Scan ----------------")
+
+    # looping untuk mendapatkan url
+    for subdomain in scan_nama_subdomain:
+
+        # membuat url dengan memasukkan subdomain satu persatu *bruteforce
+        url = f"https://{subdomain}.{scan_nama_domain}"
+
+        # memakai syntax try untuk menghindari crash pada program
+        try:
+        # sending GET request ke url
+            requests.get(url)
+        # jika setelah mencoba subdomain satu persatu valid kemudian print url nya
+            print(f"[+] {url}")
+
+    # jika url nya invalid kemudian lewatkan
+        except requests.ConnectionError:
+            pass
+# function Desc = scan_nama_subdomain in subdomain, subdomain dan scan_nama_domain in url, url got request
+
+# function utama
+if __name__ == "__main__":
+
+    # memasukkan nama domain
+    nama_dom = input("Masukkan Domain: ")
+
+    # membuka nama list file subdomain nya
+    with open("listdomain.txt","r") as file:
+
+        # membaca file nya
+        baca = file.read()
+
+        # memakai garis pemisah/spilitlines() function untuk storing list yang string nya terbelah
+        sub_dom = baca.splitlines()
+
+
+# memanggil function untuk memanggil subdo
+subscanner(nama_dom, sub_dom)
+
+
